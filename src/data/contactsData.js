6b8e08f8b00acd8a7bf0f641fcf39f5beb2b5951
Nathan <nwolf2@oswego.edu>
2022-04-22 15:35:03 -0400

    initial edits

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
-    email: 'janedoe.test@gmail.com',
-    phone: '+915588776600',
-    address: 'Menlo Park, California, United States - 673822 ',
+  email: "nwolf2@oswego.edu",
+  phone: "+5184193521",
+  address: "Oswego, NY - 13126",
 
-    sheetAPI: ''
-}
+  sheetAPI: "",
+};
