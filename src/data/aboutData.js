fa9dccc21a738b0efb9c44c2e3128a33d306a0b5
NULLWulf <89611095+NULLWulf@users.noreply.github.com>
2022-04-30 15:57:49 -0400

    updated about, hid services and blog from navbar

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
   title: "Who I am",
   description1:
-    "My name is Nathan Wolf.  I am currently a Software Engineering major in my Junior year.",
-  description2: "",
+    "Ambitious, energetic, aspiring software developer. I have a big passion for what I do and enjoy continuously learning about new technologies, frameworks, stacks and methods of tackling different solutions. I have a wide berth of experience with various programming languages but I have been finding my home most with Java and it's related languages like Kotlin and Clojure and can foresee a career working with that language. However I do pride myself in being malleable and able to quickly catch on what is required for the task, project or job, so that is not to say that I am one trick pony in that regard.  I returned to school as a non-traditional student, which has presented it's own unique privilege's and challenges, however I have found much personal satisfaction in being able to academically engage with my peers as we work and produce great things together.",
+  description2: "I of course look forward to applying all of that philosophy and ethic to working for your organization. I don't keep much a secret, feel free to take a look around at some of my projects and playthings, or connect and let's network.",
   image: 1,
 };
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,8 +2,9 @@ import resume from "../assets/pdf/resume.pdf";
 
 export const headerData = {
   name: "Nathaniel Wolf",
-  title: "Computer Science Student || Aspiring Software Engineer",
-  desciption: "~~~~~~",
+  title: "Computer Science University Student",
+  desciption:
+    "Welcome to my personal website thanks for stopping in! I am a Computer Science student finsihing my Junior year at SUNY Oswego.  Please feel free to take a look around, particularly at my professional experience as well as my projects.",
   image: "prof.jpeg",
   resumePdf: resume,
 };
