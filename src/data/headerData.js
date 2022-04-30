6c3810f73fbb2af6db83f0e23e445d53c0e56f78
NULLWulf <89611095+NULLWulf@users.noreply.github.com>
2022-04-30 16:14:56 -0400

    ~~

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,10 +1,11 @@
 import resume from "../assets/pdf/resume.pdf";
+import profileImg from "../assets/png/prof.png";
 
 export const headerData = {
   name: "Nathaniel Wolf",
   title: "Computer Science University Student",
   desciption:
     "Welcome to my personal website thanks for stopping in! I am a Computer Science student finsihing my Junior year at SUNY Oswego.  Please feel free to take a look around, particularly at my professional experience as well as my projects.",
-  image: "prof.jpeg",
+  image: profileImg,
   resumePdf: resume,
 };
