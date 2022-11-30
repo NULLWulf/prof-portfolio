822b4c30b79e78b2c8be2ae1a688d9e25abb45ad
Nathan <89611095+NULLWulf@users.noreply.github.com>
2022-11-30 17:39:27 +0000

    updated headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,9 +3,12 @@ import profileImg from "../assets/png/prof.png";
 
 export const headerData = {
   name: "Nathaniel Wolf",
-  title: "Computer Science University Student",
+  title: "Sr. Computer Science Major",
   desciption:
-    "Welcome to my personal website thanks for stopping in!  I am a Senior in Computer Science at SUNY Oswego who will be graduating in May of 2023.  Feel free to take a look around such as my LinkedIn, GitHub, and Resume.  If you have any questions or would like to get in touch with me, please use the contact form below.",
+    "Welcome to my portfolio website and thank you for visiting!" +
+    "  I am a Senior Computer Science Major at SUNY Oswego who will be graduating in May of 2023." +
+    "  Please feel free to look around, especially at my projects, experiences and LinkedIn profile." +
+    '  Click "Download CV" to view a recent copy of my resume.',
   image: profileImg,
   resumePdf: resume,
 };
